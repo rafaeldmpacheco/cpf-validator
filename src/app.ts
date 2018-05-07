8e981c3fdea36c2ccc144e91c029d01338596617
Rafael Pacheco <rafael.pacheco@senior.com.br>
2018-05-06 23:08:28 -0300

    removida validacao da rota

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,6 @@ class App {
 
     private routes(): void {
         let corsMiddleware = express.Router();
-
         corsMiddleware.use(function (req, res, next) {
             res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -39,30 +38,7 @@ class App {
             } else {
                 next();
             }
-
         });
-
-        let authMiddleware = express.Router();
-
-        authMiddleware.use((req, res, next) => {
-            let token: any = req.headers['authorization'];
-            if (token) {
-                jsonWebToken.verify(token, 'ITATAKARU', function (err) {
-                    if (err) {
-                        return res.status(401).send({
-                            message: 'Invalid token.'
-                        });
-                    } else {
-                        next();
-                    }
-                })
-            } else {
-                return res.status(403).send({
-                    message: 'No token provided.'
-                })
-            }
-        });
-
         this.express.use('/api/validate', corsMiddleware, ValidateRouter);
     }
 
